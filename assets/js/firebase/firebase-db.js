00c0a4f20e567e606aca6ac31ff0085dc72c5ed8
tripswonder <tripswondertour@gmail.com>
2026-08-06 11:51:37 +0800

    Update firebase-db.js

--- assets/js/firebase/firebase-db.js
+++ assets/js/firebase/firebase-db.js
@@ -1,1 +1,68 @@
+// =========================================
+// Firestore Database
+// =========================================
 
+import {
+
+    db
+
+} from "./firebase-config.js";
+
+import {
+
+    collection,
+
+    doc,
+
+    getDoc,
+
+    getDocs,
+
+    addDoc,
+
+    updateDoc,
+
+    deleteDoc,
+
+    setDoc,
+
+    query,
+
+    where,
+
+    orderBy
+
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
+// =========================================
+// Export Firestore Functions
+// =========================================
+
+export {
+
+    db,
+
+    collection,
+
+    doc,
+
+    getDoc,
+
+    getDocs,
+
+    addDoc,
+
+    updateDoc,
+
+    deleteDoc,
+
+    setDoc,
+
+    query,
+
+    where,
+
+    orderBy
+
+};
