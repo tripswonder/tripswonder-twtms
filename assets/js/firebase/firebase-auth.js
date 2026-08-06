7d11b2975540fb1d08c129e1e28cd58e3b5cddd0
tripswonder <tripswondertour@gmail.com>
2026-08-06 11:48:54 +0800

    Implement Firebase authentication functions

--- assets/js/firebase/firebase-auth.js
+++ assets/js/firebase/firebase-auth.js
@@ -1,1 +1,87 @@
+// =========================================
+// Firebase Authentication
+// =========================================
 
+import {
+
+    auth
+
+} from "./firebase-config.js";
+
+import {
+
+    signInWithEmailAndPassword,
+
+    createUserWithEmailAndPassword,
+
+    signOut,
+
+    onAuthStateChanged
+
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+
+// =========================================
+// LOGIN
+// =========================================
+
+export async function login(email, password) {
+
+    return await signInWithEmailAndPassword(
+
+        auth,
+
+        email,
+
+        password
+
+    );
+
+}
+
+
+// =========================================
+// REGISTER
+// =========================================
+
+export async function register(email, password) {
+
+    return await createUserWithEmailAndPassword(
+
+        auth,
+
+        email,
+
+        password
+
+    );
+
+}
+
+
+// =========================================
+// LOGOUT
+// =========================================
+
+export async function logout() {
+
+    return await signOut(auth);
+
+}
+
+
+// =========================================
+// AUTH LISTENER
+// =========================================
+
+export function authListener(callback) {
+
+    return onAuthStateChanged(
+
+        auth,
+
+        callback
+
+    );
+
+}
