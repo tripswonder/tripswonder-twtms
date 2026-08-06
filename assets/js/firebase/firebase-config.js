ce35b641415335b04bfba772e229966381257efc
tripswonder <tripswondertour@gmail.com>
2026-08-06 11:48:08 +0800

    Add Firebase configuration and initialization

--- assets/js/firebase/firebase-config.js
+++ assets/js/firebase/firebase-config.js
@@ -1,1 +1,56 @@
+// =========================================
+// Firebase SDK Imports
+// =========================================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
+// =========================================
+// Firebase Configuration
+// =========================================
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBpUXl_hgMmytwA5Y9b9eJqUtH54Z31EfY",
+
+    authDomain: "twtms-d998e.firebaseapp.com",
+
+    projectId: "twtms-d998e",
+
+    storageBucket: "twtms-d998e.firebasestorage.app",
+
+    messagingSenderId: "810095358692",
+
+    appId: "1:810095358692:web:0ff158ee3feb4e7fd172f1"
+
+};
+
+
+// =========================================
+// Initialize Firebase
+// =========================================
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+
+// =========================================
+// Export
+// =========================================
+
+export {
+
+    app,
+
+    auth,
+
+    db
+
+};
